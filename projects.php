9293eda7e1de53e7f7e9618301ff247a5c48b455
matvey <59060848+mlbuster@users.noreply.github.com>
2022-09-13 23:48:23 +0600

    Update projects.php

--- projects.php
+++ projects.php
@@ -35,6 +35,22 @@
                         </div>
                     </div>
                 </div>
+                
+                <div class="demo">
+                    <div class="project-block">
+                        <p class="project-date">2022-08-23</p>
+                        <div class="project-content">
+                            <input type="checkbox" id="hd-2" class="hide"/>
+                            <label for="hd-2" >This Page</label>
+                            <div>
+                                <p class="project-status project-status-developing">In Developing</p>
+                                <br>
+                                    This page is a portfolio created specifically for the publication of my work.
+                                    it has not been perfected and will be constantly improved.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
