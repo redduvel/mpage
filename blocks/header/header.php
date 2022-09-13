a06d97acbacdb28ee47b306815e4cc3568b24ee3
matvey <59060848+mlbuster@users.noreply.github.com>
2022-09-13 23:29:23 +0600

    Update header.php

--- blocks/header/header.php
+++ blocks/header/header.php
@@ -4,11 +4,11 @@
     </div>        
     <div class="nav-links">
         <p class="link-around"> | </p>
-        <a href="../myspace/index.php" class="nav-link">Home</a>
+        <a href="../index.php" class="nav-link">Home</a>
         <p class="link-around"> | </p>
-        <a href="../myspace/projects.php" class="nav-link">Projects</a>
+        <a href="../projects.php" class="nav-link">Projects</a>
         <p class="link-around"> | </p>
-        <a href="../myspace/blog.php" class="nav-link">Blog</a>
+        <a href="../blog.php" class="nav-link">Blog</a>
         <p class="link-around"> | </p>
         <a href="" class="nav-link">Reviews</a>
         <p class="link-around"> | </p>
