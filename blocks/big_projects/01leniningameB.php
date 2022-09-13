ff55e1fd283528b826891b17003bf3bd01450da2
matvey <59060848+mlbuster@users.noreply.github.com>
2022-09-13 23:57:18 +0600

    Create 01leniningameB.php

---
+++ blocks/big_projects/01leniningameB.php
@@ -0,0 +1,17 @@
+<div class="demo">
+                    <div class="project-block">
+                        <p class="project-date">2022-08-01</p>
+                        <div class="project-content">
+                            <input type="checkbox" id="hd-1" class="hide"/>
+                            <label for="hd-1" >Lenin In Game</label>
+                            <div>
+                                <p class="project-status project-status-developing">In Developing</p>
+                                <br>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                    Facilis incidunt necessitatibus repudiandae!
+                                    Exercitationem animi vel deleniti dicta corporis molestias minima!
+                                    Atque eum a ex dicta dignissimos? A sequi harum iure?
+                            </div>
+                        </div>
+                    </div>
+                </div>
