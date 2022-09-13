3ba6c542edb6c66ee9590f46476e02ed0d8c6975
matvey <59060848+mlbuster@users.noreply.github.com>
2022-09-13 23:57:39 +0600

    Update 00thispageB.php

---
+++ blocks/big_projects/00thispageB.php
@@ -0,0 +1,15 @@
+<div class="demo">
+                    <div class="project-block">
+                        <p class="project-date">2022-08-23</p>
+                        <div class="project-content">
+                            <input type="checkbox" id="hd-2" class="hide"/>
+                            <label for="hd-2" >This Page</label>
+                            <div>
+                                <p class="project-status project-status-developing">In Developing</p>
+                                <br>
+                                    This page is a portfolio created specifically for the publication of my work.
+                                    it has not been perfected and will be constantly improved.
+                            </div>
+                        </div>
+                    </div>
+                </div>
