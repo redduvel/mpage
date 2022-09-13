a81c635b202cf21b1a71798371bdf27f312cd270
matvey <59060848+mlbuster@users.noreply.github.com>
2022-09-13 23:43:59 +0600

    Update thispage.php

--- blocks/projects/thispage.php
+++ blocks/projects/thispage.php
@@ -1,1 +1,6 @@
-
+<div class="project-block">
+    <p class="project-date">2022-08-23</p>
+    <a href="./projects.php" class="project-name">This Page</a>
+    <hr class="white-line">
+    <p class="project-status project-status-developing">In developing</p>
+</div>
