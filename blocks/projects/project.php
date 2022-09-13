c6e7c8b4efae91d2d6e9bf2a486e28202cd58d1f
matvey <59060848+mlbuster@users.noreply.github.com>
2022-09-13 23:40:12 +0600

    Update project.php

--- blocks/projects/project.php
+++ blocks/projects/project.php
@@ -1,6 +1,6 @@
 <div class="project-block">
     <p class="project-date">2022-08-01</p>
-    <a href="./projects.php" class="project-name">Lenin In Game</a>
+    <a href="./projects.php" class="project-name">Lenin In GameВСВЫСЫВСЫВС</a>
     <hr class="white-line">
     <p class="project-status project-status-developing">In developing</p>
 </div>
