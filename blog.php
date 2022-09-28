f2fea1513a4e918d0c939eb21335a1ecc9f6b4ed
matvey <59060848+mlbustin@users.noreply.github.com>
2022-09-28 23:26:44 +0600

    Update blog.php

--- blog.php
+++ blog.php
@@ -19,56 +19,12 @@
             ?>
         </header>
     <section class="blog">
-            <p class="blog-h projects-h">Wall of thoughts</p>
-            <div class="blog-posts">
-                <div class="post">
-                    <div>
-                        <p class="project-date">2022-01-01</p>
-                    </div>
-                    <div class="post-container">
-                        <p class="project-name">Post name</p>
-                        
-                        <div class="post-text-lines">
-                            <div class="post-text">
-                                <p class="post-text-cl">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                    Provident, dolorem sapiente repudiandae tenetur eum perferendis
-                                    et aperiam obcaecati quae deserunt rem similique molestiae esse
-                                    velit iste quisquam? Exercitationem, voluptates hic!
-                                </p>
-                                <br>
-                                <p class="post-quote">
-                                    " Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                    Odit ut explicabo doloribus, assumenda vitae deserunt?
-                                    Sequi nobis quo, aut adipisci quod non aliquam.
-                                    Vel tenetur ullam eveniet doloribus incidunt veritatis? "
-                                </p>
-                                <br>
-                                <p class="post-text-cl">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                    Provident, dolorem sapiente repudiandae tenetur eum perferendis
-                                    et aperiam obcaecati quae deserunt rem similique molestiae esse
-                                    velit iste quisquam? Exercitationem, voluptates hic!
-                                </p>
-                                <br>
-                                <p class="post-text-cl">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                    Provident, dolorem sapiente repudiandae tenetur eum perferendis
-                                    et aperiam obcaecati quae deserunt rem similique molestiae esse
-                                    velit iste quisquam? Exercitationem, voluptates hic!
-                                </p>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div>
-                <div class="none-post">
-                    <p class="none-post-text">
-                        nothing here yet :(
-                    </p>
-                </div>
-            </div>
+        <p class="blog-h projects-h">Wall of thoughts</p>
+        <div class="none-post">
+            <p class="none-post-text">
+                nothing here yet :(
+            </p>
+         </div>
         </section>
     </div>
 </body>
